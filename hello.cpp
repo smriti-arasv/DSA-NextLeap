b699d9e42032501c86c91620d8154fd1570a8db6
smriti-arasv <78915137+smriti-arasv@users.noreply.github.com>
2023-06-11 08:35:38 -0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+	void helloWorld() {
+		cout<<"Hello World";
+	}
+};
+int main()
+{
+	Solution ob;
+	ob.helloWorld();
+	return 0;
+}
